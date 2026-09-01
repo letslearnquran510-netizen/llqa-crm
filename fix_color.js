739c5a73616edf506743742eb0381929a402eba8
letslearnquran510-netizen <letslearnquran510@gmail.com>
2026-09-01 09:14:33 -0600

    Add files via upload

---
+++ fix_color.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let code = fs.readFileSync('src/components/ParentMod.js', 'utf8');
+
+// Replace all instances of #1e40af with #065f46
+code = code.replace(/#1e40af/g, '#065f46');
+
+fs.writeFileSync('src/components/ParentMod.js', code);
+console.log('Replaced colors successfully');
---
+++ fix_timetable.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let code = fs.readFileSync('src/components/TimetableMod.js', 'utf8');
+
+// Fix the props
+code = code.replace(/  arPayments,\n  teacherFeedback,\n  setTeacherFeedback/g, '  arPayments');
+
+// Fix the .some() call
+code = code.replace(/const _paid = _st\.fee === "paid" \|\| \(arPayments,\n  teacherFeedback,\n  setTeacherFeedback \|\| \[\]\)\.some\(p => String\(p\.studentId\) === String\(_st\.id\)\);/g, 'const _paid = _st.fee === "paid" || (arPayments || []).some(p => String(p.studentId) === String(_st.id));');
+
+fs.writeFileSync('src/components/TimetableMod.js', code);
+console.log('Fixed TimetableMod');
